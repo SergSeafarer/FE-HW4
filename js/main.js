98d679f000b5c09793e3664b6ae928fafa96ca9e
Sergii <seregaonma@gmail.com>
2023-09-26 19:32:21 +0300

    add additional checks for the entered value

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 const secondsInHour = 3600;
-const hours = prompt('Enter the number of hours?');
+const hours = +prompt('Enter the number of hours?');
 const result = hours * secondsInHour;
-alert('Your result is: ' + result + ' seconds');
+if(hours > 0 && isNaN(hours) === false) {
+  alert('Your result is: ' + result + ' seconds');
+} else if(hours <= 0) {
+  alert('Number must be above 0');
+} else if(isNaN(hours) === true) {
+  alert('Your entered value is not a number');
+}
